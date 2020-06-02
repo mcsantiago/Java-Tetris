4c1145f5b01db98d91a20862ce9719646811e962
Matthew Santiago <carag.matthew@gmail.com>
2020-06-01 23:28:31 -0500

    Basic Collision + Shape generator

--- src/TetrisMainCanvas.java
+++ src/TetrisMainCanvas.java
@@ -1,10 +1,11 @@
+import shapes.*;
 import shapes.Shape;
-import shapes.SquareShape;
-import shapes.UnitSquare;
 import utils.GraphicsUtils;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class TetrisMainCanvas extends Canvas {
@@ -16,34 +17,73 @@ public class TetrisMainCanvas extends Canvas {
     int yPos;       // top-right of the main canvas
     int xPosMax;    // bottom-left of the main canvas
     int yPosMax;    // bottom-right of the main canvas
+    int centerX = (canvasSquareWidth / 2) - 1;
+
     int width = xPosMax - xPos;
     int height = yPosMax - yPos;
 
     ArrayList<shapes.Shape> shapes;
+    Shape activeShape;
+    Shape nextShape;
+
+    Random rng = new Random();
 
     public TetrisMainCanvas() {
         xPos = 10;
         yPos = 10;
         shapes = new ArrayList<>();
-        shapes.add(new SquareShape((canvasSquareWidth / 2) - 1, canvasSquareHeight - 1));
-//        shapes.add(new LineShape(0, 2));
-//        shapes.add(new TShape(0, 3));
-//        shapes.add(new LShape(0, 4));
+        activeShape = pickNextShape();
 
         setSize(uLength * canvasSquareWidth, uLength * canvasSquareHeight);
     }
 
     /**
      * Updates the entire canvas
-     *
-     * @param timeMs
      */
     public void updateStep() {
         // Move active square down by 1
-        System.out.println("Update called");
-        Shape activeShape = shapes.get(0);
-        for (UnitSquare square : activeShape.getSquares()) {
-            square.decCanvasY();
+        activeShape.decYPosition();
+
+        if (isCollided(activeShape)) {
+            System.out.println("Collision detected");
+            if (!activeShape.isCollidedWithFloor()) {
+                activeShape.incYPosition(); // Corrected position
+            }
+            shapes.add(activeShape);
+            activeShape = pickNextShape();
+        }
+    }
+
+    private boolean isCollided(Shape activeShape) {
+        boolean collided = false;
+        for (Shape shape : shapes) {
+            collided |= (activeShape.isCollidedWith(shape));
+        }
+        System.out.println("Collided = " + collided);
+        return collided || activeShape.isCollidedWithFloor();
+    }
+
+    /**
+     * Spawns new shape
+     */
+    private Shape pickNextShape() {
+        int nextShapeId = ThreadLocalRandom.current().nextInt(0, 4);
+        switch (nextShapeId) {
+            case 0 -> {
+                return new SquareShape(centerX, canvasSquareHeight - 1);
+            }
+            case 1 -> {
+                return new TShape(centerX, canvasSquareHeight - 1);
+            }
+            case 2 -> {
+                return new LineShape(centerX, canvasSquareHeight - 1);
+            }
+            case 3 -> {
+                return new LShape(centerX, canvasSquareHeight - 1);
+            }
+            default -> {
+                throw new IndexOutOfBoundsException("Shape not found");
+            }
         }
     }
 
@@ -51,7 +91,7 @@ public class TetrisMainCanvas extends Canvas {
      * Draws the entire canvas
      */
     public void paint(Graphics g) {
-        System.out.println("Paint call");
+//        System.out.println("Paint call");
         GraphicsUtils.drawBorder(xPos, yPos, width, height, 5, g);
 
         // Draw the debug grid lines
@@ -61,12 +101,16 @@ public class TetrisMainCanvas extends Canvas {
             }
         }
 
+        drawShape(activeShape, g);
 
-        // Current shapes
         for (Shape shape : shapes) {
-            for (UnitSquare square : shape.getSquares()) {
-                drawUnit(square, g);
-            }
+            drawShape(shape, g);
+        }
+    }
+
+    private void drawShape(Shape shape, Graphics g) {
+        for (UnitSquare square : shape.getSquares()) {
+            drawUnit(square, g);
         }
     }
 
--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -4,19 +4,53 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public abstract class Shape {
-    int xPos, yPos;
-    ArrayList<UnitSquare> squares;
+  int xPos, yPos;
+  ArrayList<UnitSquare> squares;
 
-    public Shape(int xPos, int yPos) {
-        this.xPos = xPos;
-        this.yPos = yPos;
-        squares = new ArrayList<>();
+  public Shape(int xPos, int yPos) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    squares = new ArrayList<>();
+  }
+
+  public abstract Color getColor();
+
+  public void decYPosition() {
+    yPos--;
+    for (UnitSquare square : squares) {
+      square.decCanvasY();
+    }
+  }
+
+  public ArrayList<UnitSquare> getSquares() {
+    return squares;
+  }
+
+  public boolean isCollidedWith(Shape shape) {
+    for (UnitSquare square : squares) {
+      for (UnitSquare other : shape.squares) {
+        if ((square.canvasX == other.canvasX) && (square.canvasY == other.canvasY)) {
+          return true;
+        }
+      }
     }
 
-    public abstract Color getColor();
+    return false;
+  }
 
-    public ArrayList<UnitSquare> getSquares() {
-        return squares;
+  public boolean isCollidedWithFloor() {
+    for (UnitSquare square : squares) {
+      if (square.getCanvasY() <= 0) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  public void incYPosition() {
+    yPos++;
+    for (UnitSquare square : squares) {
+      square.incCanvasY();
+    }
+  }
 }
-
